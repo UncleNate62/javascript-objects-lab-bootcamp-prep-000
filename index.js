ca849ff1457e28bd9211ea7d8afa61c0f0622620
Nathan T Case <unclenate62@gmail.com>
2018-07-01 14:56:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Initialize object "recipes"
 
-var recipes = new object{}
+var recipes = new object()
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, recipes, { [key]: value })
