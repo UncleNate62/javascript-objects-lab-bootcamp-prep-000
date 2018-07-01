2e17fb681b667962107d6abf9a132550c5754bdf
Nathan T Case <unclenate62@gmail.com>
2018-07-01 15:09:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //Initialize object "recipes"
 
-var recipes = {}
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
+  return Object.assign({}, object, { [key]: value });
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
