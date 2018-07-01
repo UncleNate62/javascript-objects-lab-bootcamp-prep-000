9d6dadb6e43c729199e3184a40dd4237b9fecb0f
Nathan T Case <unclenate62@gmail.com>
2018-07-01 15:13:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var tempObject = Object.assign({}, object)
+  delete tempObject[key]
+  return tempObject
 }
