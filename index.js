362242495612e8f2b964b9e181e57ca4ce25b7fe
Nathan T Case <unclenate62@gmail.com>
2018-07-01 15:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function deleteFromObjectByKey(object, key) {
   delete tempObject[key]
   return tempObject
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
